6e0b71199ee93a933cdbeb1eb567a094c315aaf4
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-25 23:55:04 +0400

    Added UserRepository

---
+++ src/main/java/com/company/springbootgraphqlexample/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.company.springbootgraphqlexample.repository;
+
+import com.company.springbootgraphqlexample.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
