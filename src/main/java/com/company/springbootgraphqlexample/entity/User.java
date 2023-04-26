40797065058784cd61cf13f35b000be7ab60add2
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-26 23:17:15 +0400

    Fixed

--- src/main/java/com/company/springbootgraphqlexample/entity/User.java
+++ src/main/java/com/company/springbootgraphqlexample/entity/User.java
@@ -1,21 +1,22 @@
 package com.company.springbootgraphqlexample.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
-@Document(collation = "users")
-@Getter
-@Setter
+//@Document(collation = "users")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
     @Id
     private Long id;
+    @NotBlank(message = "Username is required")
     private String username;
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is required")
     private String password;
 }
