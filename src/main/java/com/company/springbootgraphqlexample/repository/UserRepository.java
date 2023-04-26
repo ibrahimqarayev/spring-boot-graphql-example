dcd1a98f76cecf5c75576102283ab437d7cca19c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-26 23:17:22 +0400

    Fixed

--- src/main/java/com/company/springbootgraphqlexample/repository/UserRepository.java
+++ src/main/java/com/company/springbootgraphqlexample/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.company.springbootgraphqlexample.repository;
 
 import com.company.springbootgraphqlexample.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 }
