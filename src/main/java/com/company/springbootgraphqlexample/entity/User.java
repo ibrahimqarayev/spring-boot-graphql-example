8fdd452319debfe2aab0a7551dfe7925404536c7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-27 23:35:44 +0400

    Added fields

--- src/main/java/com/company/springbootgraphqlexample/entity/User.java
+++ src/main/java/com/company/springbootgraphqlexample/entity/User.java
@@ -3,6 +3,12 @@ package com.company.springbootgraphqlexample.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 //@Document(collation = "users")
 @Data
@@ -10,16 +16,49 @@ import lombok.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     private Long id;
-    @NotBlank(message = "Username is required")
     private String username;
-    @NotBlank(message = "Email is required")
     private String email;
-    @NotBlank(message = "Password is required")
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
