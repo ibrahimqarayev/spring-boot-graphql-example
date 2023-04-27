0ec81bcea8a58e97764978e050978b864460554b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-27 23:42:13 +0400

    Fixed

--- src/main/java/com/company/springbootgraphqlexample/dto/UserDto.java
+++ src/main/java/com/company/springbootgraphqlexample/dto/UserDto.java
@@ -1,12 +1,9 @@
 package com.company.springbootgraphqlexample.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
