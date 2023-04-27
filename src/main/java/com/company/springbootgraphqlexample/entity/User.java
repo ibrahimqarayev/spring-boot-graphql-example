9a7f63b66be524baad1dd5251a48b9305a921333
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-27 23:32:18 +0400

    Added field

--- src/main/java/com/company/springbootgraphqlexample/entity/User.java
+++ src/main/java/com/company/springbootgraphqlexample/entity/User.java
@@ -19,4 +19,7 @@ public class User {
     private String email;
     @NotBlank(message = "Password is required")
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
