0955a92bafef137dd14c55f035029eb7b84570da
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-27 23:38:10 +0400

    Added UserLoginDto

---
+++ src/main/java/com/company/springbootgraphqlexample/dto/UserLoginDto.java
@@ -0,0 +1,15 @@
+package com.company.springbootgraphqlexample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDto {
+    private String username;
+    private String password;
+}
