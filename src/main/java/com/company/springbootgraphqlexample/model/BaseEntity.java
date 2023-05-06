4b228ba740fbe47a6a4163d7861e292642924223
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-06 23:37:00 +0400

    Added BaseEntity

---
+++ src/main/java/com/company/springbootgraphqlexample/model/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.company.springbootgraphqlexample.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Entity
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @UpdateTimestamp
+    private LocalDateTime updated;
+
+}
