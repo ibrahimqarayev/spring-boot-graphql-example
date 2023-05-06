e658b89e329d016049d5fc42efe4fe852869c829
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-06 23:37:53 +0400

    Added User entity

---
+++ src/main/java/com/company/springbootgraphqlexample/model/User.java
@@ -0,0 +1,26 @@
+package com.company.springbootgraphqlexample.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    private String username;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
