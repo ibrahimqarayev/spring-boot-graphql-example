500a0040bebcddfff5df8ec43e4d761fbd955f33
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-06 23:37:14 +0400

    Added UserDto

---
+++ src/main/java/com/company/springbootgraphqlexample/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.company.springbootgraphqlexample.dto;
+
+import com.company.springbootgraphqlexample.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private Role role;
+}
