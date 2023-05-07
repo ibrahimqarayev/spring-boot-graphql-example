2227fb9444016f95d32e38a5c33bc6880752c1fa
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-07 20:06:53 +0400

    Added fields

--- src/main/java/com/company/springbootgraphqlexample/dto/UserDto.java
+++ src/main/java/com/company/springbootgraphqlexample/dto/UserDto.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -14,5 +17,8 @@ public class UserDto {
     private Long id;
     private String username;
     private String email;
+    private String password;
     private Role role;
+    private OffsetDateTime created;
+    private OffsetDateTime updated;
 }
