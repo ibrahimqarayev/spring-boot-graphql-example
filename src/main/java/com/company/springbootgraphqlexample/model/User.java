ce0969e3c423737df37dd9823cd7e5bd2a3b4ff2
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-07 20:06:43 +0400

    Fixed

--- src/main/java/com/company/springbootgraphqlexample/model/User.java
+++ src/main/java/com/company/springbootgraphqlexample/model/User.java
@@ -5,21 +5,19 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
-
     private String username;
     private String email;
+    private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
 
