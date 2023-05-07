3b47c9ba47bc80de940581b3eea7aeeb352a2e57
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-07 20:07:56 +0400

    Added beans

--- src/main/java/com/company/springbootgraphqlexample/SpringbootGraphqlExampleApplication.java
+++ src/main/java/com/company/springbootgraphqlexample/SpringbootGraphqlExampleApplication.java
@@ -1,7 +1,13 @@
 package com.company.springbootgraphqlexample;
 
+import graphql.scalars.ExtendedScalars;
+import graphql.schema.idl.RuntimeWiring;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
 
 @SpringBootApplication
 public class SpringbootGraphqlExampleApplication {
@@ -9,5 +15,21 @@ public class SpringbootGraphqlExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootGraphqlExampleApplication.class, args);
     }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return new RuntimeWiringConfigurer() {
+            @Override
+            public void configure(RuntimeWiring.Builder builder) {
+                builder.scalar(ExtendedScalars.DateTime);
+            }
+        };
+    }
+
 
 }
